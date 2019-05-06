484334cb9c30288b7508ecd22026607b9b0374d5
anand-jpa <anand_jpa@yahoo.com>
2019-05-06 15:46:37 +1000

    deleted

--- E-store/README.md
+++
@@ -1,3 +0,0 @@
-# This is the eStore project code.
-#dummy=vvs
-password=navin
--- E-store/jmeter/jmeter_script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp $CI_PROJECT_DIR/jmeter/estore.jmx /opt/
-/opt/apache-jmeter-4.0/bin/jmeter -n -t /opt/estore.jmx
